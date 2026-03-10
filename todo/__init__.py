c88c30dda4de583a61899da959ead71807d441ec
Smrithi V <smrithi.venugopal.v@gmail.com>
2026-03-10 13:16:35 +1000

    2026-p3-Smrithi-V

--- todo/__init__.py
+++ todo/__init__.py
@@ -8,7 +8,6 @@ def create_app(config_overrides=None):
    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite")
    if config_overrides: 
        app.config.update(config_overrides)
- 
    # Load the models 
    from todo.models import db 
    from todo.models.todo import Todo 
